debc4de352064155117af66819ffc0c26664b2de
Shpana1337 <hell.dog70@yandex.ru>
2024-03-07 23:34:42 +0300

    Начал писать тесты для jpa

--- src/test/java/ermolaev/database/repos/WorkerRepositoryTest.java
+++ src/test/java/ermolaev/database/repos/WorkerRepositoryTest.java
@@ -2,33 +2,43 @@ package ermolaev.database.repos;
 
 import ermolaev.models.abstractions.Worker;
 import ermolaev.models.impl.BackendDeveloper;
+import ermolaev.models.impl.FrontendDeveloper;
+import ermolaev.service.WorkerService;
+import jakarta.persistence.EntityManager;
 import org.hibernate.jdbc.Work;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class WorkerRepositoryTest {
+    private WorkerService workerService;
     @Autowired
-    private WorkerRepository workerRepository;
-    private Worker worker;
+    WorkerRepository workerRepository;
+    @Autowired
+    TestEntityManager testEntityManager;
 
     @BeforeEach
     void setup() {
-        worker = new BackendDeveloper("JpaTestName", "JpaTestName@mail.com");
+        testEntityManager.persistAndFlush(new BackendDeveloper("Andrew", "fylhtdrf@yandex.ru"));
+        testEntityManager.persistAndFlush(new FrontendDeveloper("Alex", "alex@email.com"));
+        workerService = new WorkerService(workerRepository);
     }
 
     @Test
-    void addNewWorker_validWorkerData_shouldReturnWorker() {
-        // given
+    void getAllWorkers() {
+        List<Worker> workerList = workerService.getAllWorkers();
+        System.out.println( "Worker list from test" + workerList);
+        assertThat(workerList).isNotNull();
 
-        // when
-        Worker savedWorker = workerRepository.save(worker);
-        // then
-        assertThat(savedWorker).isNotNull();
-        assertThat(savedWorker.getName()).isNotNull();
-        assertThat(savedWorker.getEmail()).isNotNull();
     }
 }
