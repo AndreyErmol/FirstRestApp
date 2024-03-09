b9937ac9ea4af43a5b334cab10fd75d14d794e9d
Shpana1337 <hell.dog70@yandex.ru>
2024-03-09 22:41:32 +0300

    Убрал ненужное из BootAppStarter

--- src/main/java/ermolaev/BootAppStarter.java
+++ src/main/java/ermolaev/BootAppStarter.java
@@ -9,12 +9,6 @@ import java.util.logging.LogManager;
 @SpringBootApplication
 public class BootAppStarter {
     public static void main(String[] args) {
-        try {
-            LogManager.getLogManager().readConfiguration(
-                    BootAppStarter.class.getResourceAsStream("/logging.properties"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
         SpringApplication.run(BootAppStarter.class, args);
     }
 }
